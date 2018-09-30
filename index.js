1ee00129f884d72f246dfada1a7c02f2e04d6ddb
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-09-30 15:15:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,9 +67,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  
-}
-
-function placeOrder(){
-  return "Sorry, we don't have a credit card on file for you."
+  if(cardNumber !== undefined){
+    str = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  }
+  else{
+    return "Sorry, we don't have a credit card on file for you."
+  }
 }
